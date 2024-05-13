b3e41820d20846866f8788f4aa05e3d26f58265a
Lukáš Doležal <Dolezal.Lukas@stud.vassboskovice.cz>
2024-05-13 13:20:15 +0200

    změna

--- EP1-Faktorial/Program.vb
+++ EP1-Faktorial/Program.vb
@@ -3,13 +3,27 @@
 Module Program
 
     Sub Main(args As String())
-
         Dim cislo As Integer
         Dim soucet As Integer = 0
 
         Console.Write("Zadej èíslo: ")
         cislo = Console.ReadLine
+        If cislo <= 0 Then
+            Console.Write("Chyba vstupu")
+            End
+        End If
 
+        If cislo > 10 Then
+            Console.WriteLine("musi byt mensi nez deset")
+            Console.ReadKey()
+            End
+
+        End If
+        If Not IsNumeric(cislo) Then
+            Console.WriteLine("neni to cislo")
+            Console.ReadKey()
+            End
+        End If
         For i = 1 To cislo
             soucet = soucet + i
         Next
